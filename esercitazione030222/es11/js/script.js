03bd2abf7ac462c825d997b198b6d93ad0720ff4
Giuseppe <96467406+ggarone@users.noreply.github.com>
2022-02-08 16:46:45 +0100

    Add files via upload

--- esercitazione030222/es11/js/script.js
+++ esercitazione030222/es11/js/script.js
@@ -2,7 +2,8 @@ let result = document.getElementById('result');
 
 let n1 = prompt('Inserisci numero1: ');
 let n2 = prompt('Inserisci numero2: ');
-
+n1 = parseInt(n1);
+n2 = parseInt(n2);
 if(n1>n2){
     alert(`${n1} e' maggiore di ${n2}`);
 }
