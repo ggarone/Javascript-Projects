655847f601f215e86be6dfad4d57e587aa5da14c
Giuseppe <96467406+ggarone@users.noreply.github.com>
2022-03-10 17:39:43 +0100

    Add files via upload

--- lez-04/js/02-script-advanced.js
+++ lez-04/js/02-script-advanced.js
@@ -8,41 +8,35 @@ fetch(url)
 .then(data => {return data.json()})
 .then(response => {
     dati.push(response);
-    // console.log(response);
     trasofrmaInJSON(response.data);
-    showData();
 })
 
 function trasofrmaInJSON(dati) {
-    // console.log(dati);
+    console.log(dati);
     dati.forEach(element => {
-        // document.write(element.first_name + ' ' + element.last_name + '<br>');
-
         let li = document.createElement('li');
-        let txtNode = document.createTextNode(element.id + ' ' + element.first_name + ' ' + element.last_name);
-        li.appendChild(txtNode);
+        // let txtNode = document.createTextNode(element.first_name + ' ' + element.last_name);
+        let accordionString = 
+        `
+        <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="heading${element.id}">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${element.id}" aria-expanded="true" aria-controls="collapseOne">
+                ${element.first_name + ' ' + element.last_name}
+            </button>
+          </h2>
+          <div id="collapse${element.id}" class="accordion-collapse collapse show" aria-labelledby="heading${element.id}" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>${element.email + '<img src='+element.avatar+'>'}</strong>
+            </div>
+          </div>
+        </div>
+        </div>
+        `
+        li.innerHTML = accordionString;
         lista.appendChild(li);
-        
-    });
-}
+        console.log(element);
 
-function showData() {
-    output.innerHTML = '';
-    let LIs = document.querySelectorAll('li');
-    // console.log(LIs);
-    LIs.forEach(element => {
-        let icon = document.createElement('span');
-        let X = document.createTextNode(' [x] ');
-        icon.appendChild(X);
-        element.appendChild(icon);
-    
-        icon.onclick = function () {
-            output.innerHTML = '';
-            id = Number(element.textContent.split(" ")[0]) - 1;
-            data = dati[0].data[id]
-            output.innerHTML += data.email + '<br>';
-            output.innerHTML += '<img src='+data.avatar+'>';
-        }
         
     });
 }
