db523c5713be0006cbe74591422832aeae8c7758
Giuseppe <96467406+ggarone@users.noreply.github.com>
2022-03-29 17:02:30 +0200

    Add files via upload

--- Meteo/js/meteo.js
+++ Meteo/js/meteo.js
@@ -1,13 +1,15 @@
-const apiKey = '';
+const apiKey = '839535395cb7604147768791299d89ce';
 let counter = 0;
 let cardCounter = 0;
 let id = 0;
 let LI = '';
 let flag = false;
+let temperatures = [];
+let days = [];
 
 $('#city').keyup(function (e) {
     // e.preventDefault();
-    if(e.keyCode == 13){
+    if (e.keyCode == 13) {
         let city = $('#city').val();
         const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
         console.log(url);
@@ -28,9 +30,9 @@ $('#city').keyup(function (e) {
                     let tempMax = Math.round(element.main.temp_max - 273);
                     time = time.substring(0, 5);
                     const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-    
+
                     if (counter == 0 && flag == false) {
-                        printCurrentWeather(element,iconUrl,tempMin,tempMax);
+                        printCurrentWeather(element, iconUrl, tempMin, tempMax);
                     }
                     // exit when all cards are full
                     if (counter > 19)
@@ -40,36 +42,40 @@ $('#city').keyup(function (e) {
                         cardCounter++;
                         return;
                     }
-    
+
                     // make sure card start on midnight of next day
                     if (time != '00:00' && counter == 0) {
                         return;
                     }
-    
+
+                    temperatures.push(tempMax);
+                    days.push(day);
+
                     // create new card every 5 elemtents
                     if (counter % 5 == 0) {
                         createNewCard(day);
                     }
-    
+
                     // create redundancy for midnight of next day
                     if (counter % 5 == 0 && counter != 0) {
                         $('#' + id).append(LI);
                         counter++;
                     }
-    
-                    addNewLIElement(time,tempMin,description,iconUrl);
+
+                    addNewLIElement(time, tempMin, description, iconUrl);
                     counter++;
                     cardCounter++;
                 });
+                populateChart(city);
             }
         });
     }
 });
 
 // populates UL with LI elements
-function addNewLIElement(time,tempMin,description,iconUrl) {
+function addNewLIElement(time, tempMin, description, iconUrl) {
     LI =
-    `
+        `
         <li class="list-group-item">
         <p>${time}<span class="desc">${description}</span></p>
         <p>temp: ${tempMin}°</p><img class="" src="${iconUrl}">
@@ -79,7 +85,7 @@ function addNewLIElement(time,tempMin,description,iconUrl) {
 }
 
 // prints current weather
-function printCurrentWeather(element,iconUrl,tempMin,tempMax) {
+function printCurrentWeather(element, iconUrl, tempMin, tempMax) {
     let humidity = element.main.humidity;
     let pressure = element.main.pressure;
     let main = element.weather[0].main;
@@ -104,7 +110,7 @@ function createNewCard(day) {
         id++;
     // console.log(`created a new card with counter-> ${counter} id->${id}`);
     let card =
-    `
+        `
         <div class="card" class="col-3" style="width: 18rem;">
         <h2 class="text-center">${day}</h2>
         <ul class="list-group list-group-flush" id="${id}"></ul>
@@ -115,8 +121,8 @@ function createNewCard(day) {
 }
 
 function getInitDynamic() {
-    let dy = 
-    `
+    let dy =
+        `
     <div id="meteoAttuale"></div>
     <div class="row">
         <div id="cards"></div>
@@ -127,5 +133,32 @@ function getInitDynamic() {
     id = 0;
     LI = '';
     flag = false;
+    temperatures = [];
+    days = [];
     return dy;
+}
+
+function populateChart(city) {
+    console.log(days);
+    console.log(temperatures);
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+              labels: days,
+              datasets: [{
+                  data: temperatures,
+                  label: "Temperatures",
+                  borderColor: "#3e95cd",
+                  fill: false
+                }
+              ]
+            },
+            options: {
+              title: {
+                display: true,
+                text: `Temperatures of ${city} (in degrees)`
+              }
+            }
+    });
 }
